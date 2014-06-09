6435fc46dcd06cebbafbc88ecbc52242546014a0
Lawrence Chang <lc.uclakers@gmail.com>
2014-06-08 22:34:41 -0400

    Initial commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+require_relative 'models/contact'
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+
+  @contact = Contact.find(params[:id])
+
+  erb :show
+end
